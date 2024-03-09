09024c0a49dabf8e8ebd3e560e759c395358cbf6
eman6904 <neman6904@gmail.com>
2024-03-09 05:11:40 +0200

    cc

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.task1
 
+import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.*
@@ -9,53 +11,20 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-///////////////////////////////////////////////////////////////////////
 
-        // to navigate from main activity to second activity and send data
-        val displayButton = findViewById<Button>(R.id.displayButton)
+        val title=findViewById<TextView>(R.id.title)
+        val signup=findViewById<TextView>(R.id.signup)
 
-        val name=findViewById<EditText>(R.id.name_edText)
-        val age=findViewById<EditText>(R.id.age_edText)
+        //to set underline with text
+        title.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
-        displayButton.setOnClickListener {
-
-            val intent=Intent(this,SecondActivity::class.java)
-
-            //intent.data= Uri.parse(name.text.toString())
-            //or
-            intent.putExtra("key","welcome every one")
-
-            /////////////////////////////////////////////////////////
-            //to send pair of data
-            val extras = Bundle()
-            extras.putString("name", name.text.toString())
-            extras.putString("age", age.text.toString())
-            intent.putExtras(extras)
-            startActivity(intent)
-
-        }
+        signup.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    //to show progressDialog
-    override fun onStart() {
-        super.onStart()
-        progressDialog = ProgressDialog.show(this, "Please Wait", "Processing...", true)
-        object : CountDownTimer(3000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                // Do nothing on tick
-            }
-
-            override fun onFinish() {
-                progressDialog.dismiss()
-            }
-        }.start()
-    }
 }
